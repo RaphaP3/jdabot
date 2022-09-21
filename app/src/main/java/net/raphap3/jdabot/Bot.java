d35040ec57052ed3938544f6975818aebdc6a25b
RaphaP3 <rapha.pego@gmail.com>
2022-09-21 12:30:20 -0300

    Conexão a API + arquivo Config e comando Shutdown

--- app/src/main/java/net/raphap3/jdabot/Bot.java
+++ app/src/main/java/net/raphap3/jdabot/Bot.java
@@ -1,9 +1,25 @@
 package net.raphap3.jdabot;
 
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+
+import javax.security.auth.login.LoginException;
+
 public class Bot {
 
-    public static void main(String[] args) {
-        //
+    private Bot() throws LoginException {
+
+        JDABuilder.createDefault(
+                Config.get("token")
+        )
+                .addEventListeners(new Listener())
+                .setActivity(Activity.listening("pora jaum :v"))
+                .build();
+
+    }
+
+    public static void main(String[] args) throws LoginException {
+        new Bot();
     }
 
 }
---
+++ app/src/main/java/net/raphap3/jdabot/Listener.java
@@ -0,0 +1,32 @@
+package net.raphap3.jdabot;
+
+import me.duncte123.botcommons.BotCommons;
+import net.dv8tion.jda.api.events.ReadyEvent;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Listener extends ListenerAdapter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Listener.class);
+
+    @Override
+    public void onReady(@NotNull ReadyEvent event) {
+        LOGGER.info("{} esta pronto.", event.getJDA().getSelfUser().getAsTag());
+    }
+
+    @Override
+    public void onGuildMessageReceived(@NotNull GuildMessageReceivedEvent event) {
+        String prefix = Config.get("prefix");
+        String raw = event.getMessage().getContentRaw();
+
+        if (raw.equalsIgnoreCase(prefix + "shutdown")
+                && event.getAuthor().getId().equals(Config.get("owner_id"))) {
+            LOGGER.info("Desligando");
+            event.getJDA().shutdown();
+            BotCommons.shutdown(event.getJDA());
+        }
+    }
+}
